73a4e3b365ca3c90ef6db16c71c7fa178a36201f
TageDan <danielsson.dev@gmail.com>
2025-01-11 18:05:17 +0100

    Addition to last commit message: Also greatly simplified the unmake move method. Even though this might impact performance. Something to maybe redo again later.

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,21 @@ fn main() {
     loop {
         let mut input = String::new();
         stdin.read_line(&mut input);
-        let cmove: Result<Move, &str> = input.parse();
+        if input.trim() == "u" {
+            board.unmake_last();
+        } else {
+            let cmove: Result<Move, &str> = input.parse();
 
-        if cmove.is_ok() {
-            let res = board.make_move(cmove.clone().unwrap());
-            if res.is_err() {
-                println!("error making move: {:?}", res);
-                let _ = stdin.read_line(&mut String::new());
+            if cmove.is_ok() {
+                let res = board.make_move(&cmove.clone().unwrap());
+                if res.is_err() {
+                    println!("error making move: {:?}", res);
+                    let _ = stdin.read_line(&mut String::new());
+                }
             }
         }
 
         println!("\x1b[2J\x1b[H");
         println!("{}", board);
-        println!("move string: {input}");
-        println!("move: {:?}", cmove);
     }
 }
